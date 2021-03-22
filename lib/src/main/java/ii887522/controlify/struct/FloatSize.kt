3018cf091947090c31dd1dc2834426bc8c3e2386
Chi Min Yong <chimin.yong@g2g.com>
2021-03-22 20:16:19 +0800

    Add Size structs

---
+++ lib/src/main/java/ii887522/controlify/struct/FloatSize.kt
@@ -0,0 +1,14 @@
+package ii887522.controlify.struct
+
+import ii887522.oxy.any.Calculatable
+
+data class FloatSize(val w: Float = .0f, val h: Float = .0f) : Calculatable<FloatSize> {
+  override fun plus(that: FloatSize) = FloatSize(w + that.w, h + that.h)
+  override fun minus(that: FloatSize) = FloatSize(w - that.w, h - that.h)
+  override fun times(that: FloatSize) = FloatSize(w * that.w, h * that.h)
+  override fun div(that: FloatSize) = FloatSize(w / that.w, h / that.h)
+  override fun plus(that: Number) = FloatSize(w + that.toFloat(), h + that.toFloat())
+  override fun minus(that: Number) = FloatSize(w - that.toFloat(), h - that.toFloat())
+  override fun times(that: Number) = FloatSize(w * that.toFloat(), h * that.toFloat())
+  override fun div(that: Number) = FloatSize(w / that.toFloat(), h / that.toFloat())
+}
