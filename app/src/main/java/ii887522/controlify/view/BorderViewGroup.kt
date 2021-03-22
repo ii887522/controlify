bf598575ff46842760a4a0ab6de247669c18ad32
Chi Min Yong <chimin.yong@g2g.com>
2021-03-22 20:48:59 +0800

    Add BorderViewGroup

---
+++ app/src/main/java/ii887522/controlify/view/BorderViewGroup.kt
@@ -0,0 +1,35 @@
+package ii887522.controlify.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.View
+import ii887522.controlify.any.Map
+import ii887522.controlify.control.BorderView
+import ii887522.controlify.enums.Input
+import ii887522.controlify.struct.Border
+import ii887522.controlify.struct.IntPoint
+import ii887522.controlify.struct.IntRect
+import ii887522.controlify.struct.IntSize
+
+class BorderViewGroup(context: Context, attr: AttributeSet) : View(context, attr) {
+  private lateinit var borderViews: Array<BorderView<Input>>
+
+  override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+    super.onSizeChanged(w, h, oldw, oldh)
+    borderViews = arrayOf(
+      BorderView(Border(IntRect(IntPoint(0, 0), IntSize(4, 4)), 1)),
+      BorderView(Border(IntRect(IntPoint(8, 0), IntSize(4, 4)), 1)),
+      BorderView(Border(IntRect(IntPoint(16, 0), IntSize(8, 8)), 1)),
+      BorderView(Border(IntRect(IntPoint(28, 0), IntSize(8, 8)), 2)),
+      BorderView(Border(IntRect(IntPoint(40, 0), IntSize(8, 8)), 2), Color.RED),
+      BorderView(Border(IntRect(IntPoint(52, 0), IntSize(8, 8)), 2), Color.RED, Map(IntSize(4, 4)), Input.UP),
+      BorderView(Border(IntRect(IntPoint(64, 0), IntSize(8, 8)), 2), Color.RED, Map(IntSize(6, 6)), Input.RIGHT)
+    )
+  }
+
+  override fun onDraw(canvas: Canvas?) {
+    for (borderView in borderViews) borderView.render(canvas!!)
+  }
+}
