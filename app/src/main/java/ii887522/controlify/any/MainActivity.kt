bf91d89284321a9b48df011fd423389f1aec0de1
Chi Min Yong <chimin.yong@g2g.com>
2021-03-22 22:24:55 +0800

    Add main activity

---
+++ app/src/main/java/ii887522/controlify/any/MainActivity.kt
@@ -0,0 +1,42 @@
+package ii887522.controlify.any
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
+import ii887522.controlify.R
+import ii887522.controlify.databinding.ActivityMainBinding
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+  private val canIncrementScore0 = MutableLiveData(false)
+  private val canResetScore0 = MutableLiveData(false)
+  private val canIncrementScore1 = MutableLiveData(false)
+  private val canResetScore1 = MutableLiveData(false)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    ScoreModel(this, 0, { }, canIncrement = canIncrementScore0, canReset = canResetScore0).value.observe(this) {
+      binding.score0.text = resources.getString(R.string.score_text, it)
+    }
+    ScoreModel(this, 1, { }, canIncrement = canIncrementScore0, canReset = canResetScore0).value.observe(this) {
+      binding.score1.text = resources.getString(R.string.score_text, it)
+    }
+    ScoreModel(this, 1, {
+      Timber.i("You win!")
+    }, canIncrement = canIncrementScore0, canReset = canResetScore0).value.observe(this) {
+      binding.score2.text = resources.getString(R.string.score_text, it)
+    }
+    ScoreModel(this, 1, {
+      Timber.i("You win!")
+    }, canIncrement = canIncrementScore1, canReset = canResetScore0).value.observe(this) {
+      binding.score3.text = resources.getString(R.string.score_text, it)
+    }
+    ScoreModel(this, 1, {
+      Timber.i("You win!")
+    }, canIncrement = canIncrementScore1, canReset = canResetScore1).value.observe(this) {
+      binding.score4.text = resources.getString(R.string.score_text, it)
+    }
+  }
+}
