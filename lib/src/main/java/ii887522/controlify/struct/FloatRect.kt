dc7cb5e13b09e34e1b976d5fb6a8de4cf9269d8f
Chi Min Yong <chimin.yong@g2g.com>
2021-03-22 20:16:27 +0800

    Add Rect structs

---
+++ lib/src/main/java/ii887522/controlify/struct/FloatRect.kt
@@ -0,0 +1,3 @@
+package ii887522.controlify.struct
+
+data class FloatRect(val position: FloatPoint = FloatPoint(), val size: FloatSize = FloatSize())
---
+++ lib/src/main/java/ii887522/controlify/struct/IntRect.kt
@@ -0,0 +1,3 @@
+package ii887522.controlify.struct
+
+data class IntRect(val position: IntPoint = IntPoint(), val size: IntSize = IntSize())
